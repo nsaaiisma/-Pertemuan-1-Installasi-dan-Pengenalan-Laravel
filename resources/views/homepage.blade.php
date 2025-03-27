9a1107daae617fd03ee3bab37dd5ef73a36b4131
nsaaiisma <fiismatunnissa@gmail.com>
2025-03-27 19:17:51 +0700

    latihan2

---
+++ resources/views/homepage.blade.php
@@ -0,0 +1,80 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  </head>
+  <body>
+  <div style='text-align: center; font-size: 24px; font-weight: bold;'>
+            Selamat Datang Di Halaman Utama The Scott Resort 
+            <br><br>
+            <a href='/Stay' style='text-decoration: none; font-size: 20px; color: green;'>
+                01 Stay
+            </a>
+            <br><br>
+            <a href='/Eat&Drink' style='text-decoration: none; font-size: 20px; color: green;'>
+                02 Eat&Drink
+            </a>
+            <br><br>
+            <a href='/Spa&Wellness' style='text-decoration: none; font-size: 20px; color: green;'>
+                03 Spa&Wellness
+            </a>
+            <br><br>
+            <a href='/Pools' style='text-decoration: none; font-size: 20px; color: green;'>
+                04 Pools
+            </a>
+            <br><br>
+            <a href='/Location' style='text-decoration: none; font-size: 20px; color: green;'>
+                05 Location
+            </a>
+            <br><br>
+            <a href='/Meetings&Events' style='text-decoration: none; font-size: 20px; color: green;'>
+                06 Meetings&Events
+            </a>
+            <br><br>
+            <a href='/Weddings' style='text-decoration: none; font-size: 20px; color: green;'>
+                07 Weddings
+            </a>
+            <br><br>
+            <a href='/What's On' style='text-decoration: none; font-size: 20px; color: green;'>
+                08 What's On
+            </a>
+            <br><br>
+            <a href='/Packages' style='text-decoration: none; font-size: 20px; color: green;'>
+                09 Packages
+            </a>
+            <br><br>
+            <a href='/Gallery' style='text-decoration: none; font-size: 20px; color: green;'>
+                010 Gallery
+            </a>
+        </div>
+        <div>
+            <a href='https://www.thescottresort.com/rooms/' style='text-align: center; text-decoration: none; font-size: 20px; color: black; font-weight: bold'>
+                Lihat Selengkapnya:
+            </a>
+        </div>
+    <div class="card" style="width: 18rem;">
+  <img src="..." class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+  </div>
+  <ul class="list-group list-group-flush">
+    <li class="list-group-item">An item</li>
+    <li class="list-group-item">A second item</li>
+    <li class="list-group-item">A third item</li>
+  </ul>
+  <div class="card-body">
+    <a href="#" class="card-link">Card link</a>
+    <a href="#" class="card-link">Another link</a>
+  </div>
+</div>
+
+<div class="alert alert-primary" role="alert">
+  A simple primary alert—check it out!
+</div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+  </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -3,58 +3,10 @@
 use Illuminate\Support\Facades\Route;
 use Livewire\Volt\Volt;
 
+
 Route::get('/Home', function () {
-    return "
-        <div style='text-align: center; font-size: 24px; font-weight: bold;'>
-            Selamat Datang Di Halaman Utama The Scott Resort 
-            <br><br>
-            <a href='/Stay' style='text-decoration: none; font-size: 20px; color: green;'>
-                01 Stay
-            </a>
-            <br><br>
-            <a href='/Eat&Drink' style='text-decoration: none; font-size: 20px; color: green;'>
-                02 Eat&Drink
-            </a>
-            <br><br>
-            <a href='/Spa&Wellness' style='text-decoration: none; font-size: 20px; color: green;'>
-                03 Spa&Wellness
-            </a>
-            <br><br>
-            <a href='/Pools' style='text-decoration: none; font-size: 20px; color: green;'>
-                04 Pools
-            </a>
-            <br><br>
-            <a href='/Location' style='text-decoration: none; font-size: 20px; color: green;'>
-                05 Location
-            </a>
-            <br><br>
-            <a href='/Meetings&Events' style='text-decoration: none; font-size: 20px; color: green;'>
-                06 Meetings&Events
-            </a>
-            <br><br>
-            <a href='/Weddings' style='text-decoration: none; font-size: 20px; color: green;'>
-                07 Weddings
-            </a>
-            <br><br>
-            <a href='/What's On' style='text-decoration: none; font-size: 20px; color: green;'>
-                08 What's On
-            </a>
-            <br><br>
-            <a href='/Packages' style='text-decoration: none; font-size: 20px; color: green;'>
-                09 Packages
-            </a>
-            <br><br>
-            <a href='/Gallery' style='text-decoration: none; font-size: 20px; color: green;'>
-                010 Gallery
-            </a>
-        </div>
-        <div>
-            <a href='https://www.thescottresort.com/rooms/' style='text-align: center; text-decoration: none; font-size: 20px; color: black; font-weight: bold'>
-                Lihat Selengkapnya:
-            </a>
-        </div>
-    ";
-});
+    return view('homepage');
+})->name('Homepage');
     
 Route::get('/Stay', function () {
     return "Selamat datang di halaman Stay";
@@ -100,9 +52,6 @@ Route::get('/', function () {
     return view('welcome');
 })->name('home');
 
-Route::get('/latihan2', function () {
-    return view('lat2');
-})->name('latihan2');
 
 
 Route::view('dashboard', 'dashboard')
